a4f22f55d8f913f55ef05e1a5426aa4c0caf9db8
Rafael Costa <rafaelcostat@Rafaels-iMac-Pro.local>
2024-03-27 14:29:54 -0300

    update nextConfig

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { i18n } = require('./next-i18next.config.js')
 
+const repo = 'grabh'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
+  basePath,
+  assetPrefix,
   reactStrictMode: true,
   i18n,
 }
